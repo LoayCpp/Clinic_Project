f455e231e65e76e6602e3885890632d3f4ddac46
Loay Alarify <loayalarify777184509@gmail.com>
2026-03-06 19:04:44 +0300

    feat(MainMenueScreen): added class MainMenue and implement methods

--- Clinic.cpp
+++ Clinic.cpp
@@ -2,6 +2,7 @@
 #include "ProjectFiles//Core//clsDoctor.h"
 #include"ProjectFiles//Core//clsPatient.h"
 #include"ProjectFiles/Core/clsAppointment.h"
+#include"ProjectFiles//Screens\MainMenu/clsMainMenu.h"
 using namespace std;
 
 string ConvertEnGenderToStringDoctor(clsDoctor::enGender Gender)
@@ -44,13 +45,15 @@ void PrintPaitentInformation(clsPatient patient)
 int main()
 {
 
-	clsDoctor doctor("120", "Luai", "Anwar", "Fesail", "Ahemd", clsDoctor::enGender::eMale, clsDate(21, 2, 2003), "Dentist", "777184509", 10.02f);
+	/*clsDoctor doctor("120", "Luai", "Anwar", "Fesail", "Ahemd", clsDoctor::enGender::eMale, clsDate(21, 2, 2003), "Dentist", "777184509", 10.02f);
 	clsPatient patient("11", "Amr", "Ahmed", "khaled", "Smai", clsPatient::enGender::eMale, clsDate(2, 3, 2022), "932974927", doctor, 1200);
 
 	cout << "Here class Docotor:\n";
 	PrintDoctorInformation(doctor);
 	cout << "===========================\n";
 	cout << "\nHere class Patient:\n";
-	PrintPaitentInformation(patient);
+	PrintPaitentInformation(patient);*/
+	clsMainMenu::ShowMenu();
+
 	return 0;
 }
