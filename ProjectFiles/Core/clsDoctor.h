48450642ed0d5e0f5729d3ef2be71f9db98d7208
Loay Alarify <loayalarify777184509@gmail.com>
2026-03-13 04:36:56 +0300

    feat(doctor): add function to auto-increment doctor id

--- ProjectFiles/Core/clsDoctor.h
+++ ProjectFiles/Core/clsDoctor.h
@@ -1,6 +1,10 @@
 ﻿#pragma once
 #include "..//Libraries/clsDate.h"
 #include"..//Core/clsPerson.h"
+#include "..//Database/FilesName.h"
+#include<vector>
+#include<fstream>
+#include"..//Libraries/clsString.h"
 class clsDoctor  : public clsPerson
 {
 
@@ -30,6 +34,8 @@ private:
         DataisUnSaved
     };
 
+
+
     enMode _mode;
     enIsSave _ObjectIsSaved;
     string _doctorID;
@@ -38,13 +44,85 @@ private:
     string _specialization;
     float _feesRate;
 
+     vector<string> _LoadDoctorsFromFiles() {
+
+        vector<string> vDoctors;
+        fstream fileOfDoctor;
+        string dataOfLine;
+        fileOfDoctor.open(DoctorsFile, ios::in);
+
+        if (fileOfDoctor.is_open()) {
+
+            while (getline(fileOfDoctor, dataOfLine)) {
+
+
+                vDoctors.push_back(dataOfLine);
+
+
+            }
+            fileOfDoctor.close();
+        }
+        else {
+
+            cout << "\nfile it is not open\n";
+        }
+
+        return vDoctors;
+
+
+    }
+
+
+
+     int  _GetDoctorNumber() {
+
+
+        vector<string> vDoctors = _LoadDoctorsFromFiles();
+        //Doc001#//#Amr Siaf Ahmed Ali#//#1975-2-23#//#male#//#50#//#Dentistry#//#7383199920
+        //Doc002#//#Luai Anwar Fesial#//#2004-3-12#//#male#//#18#//#Dermatology#//#777123456
+
+
+        vector<string> vLineFromEndDoctor = clsString::SpilitString(vDoctors.back(), "#//#");
+
+        //Doc002 
+        //Luai Anwar Fesial
+        //2004-3-12
+
+
+        vector<string>vId = clsString::SpilitString(vLineFromEndDoctor.at(0), "Doc00");
+        //First Doc002 
+        //Second Doc00 to split 
+
+        // End 2 in to vId index[0]
+
+        int id = stoi(vId.at(0));
+
+        //convert number from string to int 
+
+        return id;
+
+
+    }
+
+     string _GeneratingIDForObject() {
+
+
+        const string doctorIdWord = "Doc00";
+        int doctorIdNum = _GetDoctorNumber();
+        ++doctorIdNum;
+
+        return doctorIdWord + to_string(doctorIdNum);
+
+    }
+
+
 public:
     clsDoctor(){}
 
     clsDoctor(enMode mode, string firstName, string secondName, string thirdName,
               string fourthName, enGender gender, clsDate birthdate, string specialization, string phone, float feesRate)
         :clsPerson(firstName,secondName,thirdName,fourthName,phone){
-
+        _doctorID = _GeneratingIDForObject();
         _mode = mode;
         _ObjectIsSaved = enIsSave::DataisUnSaved;
         _birthdate = birthdate;
