685f2823901167c1576ce2468838c0160ff20b77
Loay Alarify <loayalarify777184509@gmail.com>
2026-04-02 06:40:49 +0300

    refactor(Template): rename function and add function it is name save

--- ProjectFiles/Core/clsTemplate.h
+++ ProjectFiles/Core/clsTemplate.h
@@ -21,77 +21,68 @@ public:
         DataisUnSaved
     };
   
-    static map<string, T> LoadFromFiles(string fileName, function<T(string)> functionConvertDateToLine, function<string(T)> getID){
-
-       
-        map<string, T> mObject;
-
-        fstream fileOfDoctor;
+    static map<string, T> LoadObjectsDataFromFiles(string fileName,
+        function<T(string, string)> convertDateLineToObject,
+        function<string(const T&)> getID) {
 
 
+        map<string, T> mObjects;
 
+        fstream filesDB;
 
-        fileOfDoctor.open(fileName, ios::in);
+        filesDB.open(fileName, ios::in);
 
-        if (fileOfDoctor.is_open()) {
-        string dataOfLine;
+        if (filesDB.is_open()) {
+            string dataOfLine;
 
-            while (getline(fileOfDoctor, dataOfLine)) {
+            while (getline(filesDB, dataOfLine)) {
 
-
-
-                T object = functionConvertDateToLine(dataOfLine);
-
-
-                mObject[getID(object)] = object;
+                T object = convertDateLineToObject(dataOfLine, "#//#");
+                mObjects[getID(object)] = object;
 
             }
 
-            fileOfDoctor.close();
+            filesDB.close();
         }
         else {
 
             cout << "\nfile it is not open\n";
         }
 
-        return mObject;
+        return mObjects;
 
     }
 
-    static void  SaveDataToFile(string fileName, const map<string, T>& ObjectDataMap, function < enMode  (const T&) > Mode,function<string ( const T&) > convertObjectToDataLine ) {
+    static void  SaveObjectsDataToFile(string fileName, const map<string, T>& mObjects,
+        function < enMode(const T&) > objectMode, function<string(const T&, string) > convertObjectToDataLine) {
 
-        fstream fileOfDoctor;
+        fstream fileDB;
 
+        fileDB.open(fileName, ios::out);
 
-
-        fileOfDoctor.open(fileName, ios::out);
-
-        if (fileOfDoctor.is_open()) {
-
-
-            for (const auto& D : ObjectDataMap) {
+        if (fileDB.is_open()) {
 
 
-                if (Mode(D.second) == enMode::eUpadateMode) {
+            for (const auto& object : mObjects) {
 
-                    fileOfDoctor << convertObjectToDataLine(D.second) << "\n";
+
+                if (objectMode(object.second) == enMode::eUpadateMode) {
+
+                    fileDB << convertObjectToDataLine(object.second, "#//#") << "\n";
                 }
             }
 
-            fileOfDoctor.close();
+            fileDB.close();
         }
-      
-
-
 
 
 
     }
 
-    static string  GetEndNumberFromFile(function< map<string ,T> () > LoadsFromFiles) {
+    static string  GetEndNumberFromFile(function< map<string, T>() > loadObjectsDataFromFiles, short ConstIDNumbers = 5) {
 
 
-        map<string, T> mObject = LoadsFromFiles();
+        map<string, T> mObject = loadObjectsDataFromFiles();
 
 
         if (mObject.empty())
@@ -102,64 +93,115 @@ public:
         string WordId = mEndObject->first;
 
 
-        string id = WordId.substr(5);
+        string id = WordId.substr(ConstIDNumbers);
 
 
 
         return to_string(stoi(id) + 1);
 
     }
-   static void UpdateObject(function< map<string,T>() > loadsFromFiles ,T Object, function<string (T)> getID, function<void (const map<string,T> &)> saveToFile,enIsSave &objectIsSave) {
 
-        map < string, T> mObjects = loadsFromFiles();
+    static void UpdateObject(function< map<string, T>() > loadObjectsDataFromFiles,
+                            T Object,
+                            function<string(T)> getID,
+                            function<void(const map<string, T>&)> saveObjectsDataToFile,
+                            enIsSave& objectIsSave) {
+
+        map < string, T> mObjects = loadObjectsDataFromFiles();
 
         mObjects[getID(Object)] = Object;
 
-        saveToFile(mObjects);
+        saveObjectsDataToFile(mObjects);
         objectIsSave = enIsSave::DataisSaved;
     }
-  static void AddObjectToFile(string fileName,T object ,function<string(T)>convertObjectToDataLine,enIsSave &objectIsSave) {
-
-       fstream fileOfObject;
-
-       fileOfObject.open(fileName, ios::app);
-
-       if (fileOfObject.is_open()) {
-
- 
-           fileOfObject << convertObjectToDataLine(object) << endl;
-
-       }
-
-       fileOfObject.close();
-       objectIsSave = enIsSave::DataisSaved;
-
-   }
-  static T FindObject(string objectID,function<map<string,T> ()>loadsFromFiles,function<T ()> getEmptyObject) {
-
-      map < string, T> mObject = loadsFromFiles();
-
-      if (mObject.find(objectID) != mObject.end()) {
 
 
-          return mObject[objectID];
-      }
-      return getEmptyObject();
-  }
+    static void AddObjectToFile(string fileName,
+                                T object,
+                                function<string(const T&, string)>convertObjectToDataLine,    
+                                enIsSave& objectIsSave) {
 
-   static bool DeleteObject(T &object,enMode &mode,enIsSave &objectIsSave, function<void()> UpdateObject, function<T()>getEmptyObject) {
+        fstream fileDB;
 
-     mode = enMode::eEmptyMode;
+        fileDB.open(fileName, ios::app);
 
-     UpdateObject();
+        if (fileDB.is_open()) {
 
-      if (objectIsSave == enIsSave::DataisSaved)
-          object = getEmptyObject();
 
-      return true;
+            fileDB << convertObjectToDataLine(object,"#//#") << endl;
 
-      return false;
+        }
 
-  }
+        fileDB.close();
+        objectIsSave = enIsSave::DataisSaved;
+
+    }
+
+    static T FindObject(string objectID,    
+                        function<map<string, T>()> loadObjectsDataFromFiles,
+                        function<T()> getEmptyObject) {
+
+        map < string, T> mObject = loadObjectsDataFromFiles();
+
+        if (mObject.find(objectID) != mObject.end()) {
+
+
+            return mObject[objectID];
+        }
+        return getEmptyObject();
+    }
+
+    static bool DeleteObject(T& object,
+                            enMode& mode,
+                            enIsSave& objectIsSave,
+                            function<void()> UpdateObject,
+                            function<T()>getEmptyObject) {
+
+        mode = enMode::eEmptyMode;
+
+        UpdateObject();
+
+        if (objectIsSave == enIsSave::DataisSaved) {
+            object = getEmptyObject();
+            return true;
+        }
+
+
+        return false;
+
+    }
+    static bool Save(enMode& mode,  
+                    function<void()> UpdateObject,
+                    function<void()> AddNewObject,
+                    enIsSave &objectIsSave) {
+
+        switch (mode)
+        {
+
+        case enMode::eEmptyMode:
+            return false;
+            break;
+
+        case enMode::eUpadateMode:
+
+            UpdateObject();
+            break;
+
+
+        case enMode::eAddNewMode:
+
+            mode = enMode::eUpadateMode;
+            AddNewObject();
+
+            break;
+
+
+        }
+
+
+        return (objectIsSave == enIsSave::DataisSaved);
+
+    }
+
 };
 
