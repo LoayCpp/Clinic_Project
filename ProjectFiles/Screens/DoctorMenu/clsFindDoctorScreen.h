2cd4911cc7037e746241f382b9af0c6f74eda561
amr <amralamrei65@gmail.com>
2026-04-12 13:43:48 +0300

    feat(Find-Doctor-Screen): Create clsFindDoctorScreen class

---
+++ ProjectFiles/Screens/DoctorMenu/clsFindDoctorScreen.h
@@ -0,0 +1,13 @@
+#include "..//clsHeaderScreen.h"
+#include "clsDoctorHelperFunctions .h"
+class clsFindDoctorScreen : public clsHeaderScreen {
+
+public:
+	static void ShowFindDoctor() {
+		string title = clsUtil::Tabs(1) + "      Find Doctor Screen";
+		ShowMainHeader(title);
+		clsDoctor doctor = clsDoctorHelperFunctions::ReadDoctorByID();
+		clsDoctorHelperFunctions::PrintDoctorInfo(doctor);
+	}
+	
+};
