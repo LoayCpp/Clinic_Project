663f945f5ae8916600f03303d2df533ceec9a1d7
Loay Alarify <loayalarify777184509@gmail.com>
2026-04-12 12:12:55 +0300

    feat(add-doctor-screen): add class clsAddDoctorScreen to add doctors

---
+++ ProjectFiles/Screens/DoctorMenu/clsAddDoctorScreen.h
@@ -0,0 +1,23 @@
+#include "..//clsHeaderScreen.h"
+#include "clsDoctorHelperFunctions .h"
+class clsAddDoctorScreen : public clsHeaderScreen {
+private:
+
+public:
+	static void ShowAddDoctor() {
+		string title = clsUtil::Tabs(1) + "      Add Doctor Screen";
+		ShowMainHeader(title);
+
+		clsDoctor newDoctor = clsDoctor::GetNewDoctorObject();
+
+		clsDoctorHelperFunctions::ReadDoctorData(newDoctor);
+		clsDoctorHelperFunctions::PrintDoctorInfo(newDoctor);
+
+		if (!clsInputValidate::CheckAnswer("Are you sure you want to perform this operation? [Y/N] ")) {
+			cout << "Operation was cancelled.\n";
+		}
+
+
+		clsSharedHelperFunctions::PrintTransactionStatus(newDoctor.Save(), "Doctor", "Added");
+	}
+};
