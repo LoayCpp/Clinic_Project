337b1b7e740d5acce0efe40249e265cbf75e997e
amr <amralamrei65@gmail.com>
2026-04-29 18:31:32 +0300

    feat(clsAppointment): extend the behavior of clsAppointment class

--- ProjectFiles/Core/clsAppointment.h
+++ ProjectFiles/Core/clsAppointment.h
@@ -3,68 +3,173 @@
 class clsAppointment
 {
 private:
+
+    clsTemplate<clsAppointment>::enMode  _mode;
+    clsTemplate<clsAppointment>::enIsSave _objectIsSaved;
     string _appointmentID;
     clsDoctor _doctor;
     clsPatient _patient;
-     string _dateTime;
+    string _dateTime;
 
+    float _appointmentFees;
+    float _basePrice;
+    static clsAppointment _ConvertDataLineToAppoinment(string dataLine, string sperator = "#//#") {
+
+        vector<string> vLine = clsString::SpilitString(dataLine, sperator);
+        //Appoin001#//#Patien001#//#Doc004#//#11200#//#4/22/2026 - 2:03
+
+        return clsAppointment(clsTemplate<clsAppointment>::enMode::eUpadateMode,
+            vLine[0], clsPatient::FindPatient(vLine[1]), clsDoctor::FindDoctor(vLine[2]), stof(vLine[3]), vLine[4]);
+    }
+    string _ConvertAppointmentToDataLine(const clsAppointment& appointment, string sperator = "#//#") {
+        string line = "";
+
+        line = appointment._appointmentID + sperator;
+        line += appointment._patient.PatientID + sperator;
+        line += appointment._doctor.DoctorID + sperator;
+        line += to_string(appointment._appointmentFees) + sperator;
+        line += appointment._dateTime;
+
+        return line;
+    }
+    static string _GetAppointmentID(const clsAppointment& appointment) {
+        return appointment._appointmentID;
+    }
+    clsTemplate<clsAppointment>::enMode _GetAppointmentMode(const clsAppointment& appointment) {
+        return appointment._mode;
+    }
+    static map<string, clsAppointment> _loadAppointmentDataFromFile() {
+        return clsTemplate<clsAppointment>::LoadObjectsDataFromFiles(AppointmentsFile, _ConvertDataLineToAppoinment, _GetAppointmentID);
+    }
+    void _SaveAppointmentDataToFile(const map<string, clsAppointment>& mAppointments) {
+
+        clsTemplate<clsAppointment>::SaveObjectsDataToFile(AppointmentsFile, mAppointments,
+            [this](const clsAppointment& appointment) -> clsTemplate<clsAppointment>::enMode {return _GetAppointmentMode(appointment); },
+            [this](const clsAppointment& appointment, string sperator = "#//#") -> string
+            {return _ConvertAppointmentToDataLine(appointment, sperator); });
+    }
+    string _GetNextAppointmentNumber() {
+        //Appoin00 ---> 8 
+        return clsTemplate<clsAppointment>::GetEndNumberFromFile(_loadAppointmentDataFromFile,8);
+    }
+    void _GenerateAppointmentID() {
+        this->_appointmentID += _GetNextAppointmentNumber();
+    }
+    void _AddAppointmentToFile() {
+        clsTemplate<clsAppointment>::AddObjectToFile(AppointmentsFile, *this, 
+            [this](const clsAppointment& appointment, string sperator = "#//#") -> string
+            {return _ConvertAppointmentToDataLine(appointment, sperator); },
+            [this]() -> void {_GenerateAppointmentID(); },
+            this->_objectIsSaved);
+    }
+    void _UpdateAppointment() {
+        clsTemplate<clsAppointment>::UpdateObject(_loadAppointmentDataFromFile, *this, _GetAppointmentID,
+            [this](const map<string, clsAppointment>& mAppointments)-> void {_SaveAppointmentDataToFile(mAppointments); }, this->_objectIsSaved
+        );
+    }
+
+    clsAppointment(clsTemplate<clsAppointment>::enMode mode,
+        string appointmentID, clsPatient patient, clsDoctor doctor, float appointmentFees,string dateTime) {
+
+        this->_mode = mode;
+        this->_appointmentID = appointmentID;
+
+        this->_patient = patient;
+        this->_doctor = doctor;
+        
+        this->_appointmentFees = appointmentFees;
+        this->_dateTime = dateTime;
+        this->_basePrice = 3500;
+        this->_objectIsSaved = clsTemplate<clsAppointment>::enIsSave::DataisUnSaved;
+    }
 public:
 
-    clsAppointment(string appointmentID, clsPatient patient, clsDoctor doctor, string dateTime)
-    {
-        _appointmentID = appointmentID;
-        _patient = patient;
-        _doctor = doctor;
-        _dateTime = dateTime;
-    }
+    clsAppointment() :
+        clsAppointment(clsTemplate<clsAppointment>::enMode::eEmptyMode, "Appoin00", clsPatient(), clsDoctor(), 0.0, clsDate::GetLocalDatetime()) {}
+
+    clsAppointment(clsTemplate<clsAppointment>::enMode mode,
+        clsPatient patient, clsDoctor doctor, float appointmentFees)
+        : clsAppointment(mode,"Appoin00", patient, doctor, appointmentFees, clsDate::GetLocalDatetime())
+    {   }
 
    
-    string GetAppointmentID()
-    {
+    string GetAppointmentID() const {
         return _appointmentID;
     }
-
     __declspec(property(get = GetAppointmentID)) string AppointmentID;
 
 
     
-    clsDoctor GetDoctor()
+    clsDoctor GetDoctor() const 
     {
         return _doctor;
     }
-
     void SetDoctor(clsDoctor doctor)
     {
         _doctor = doctor;
     }
-
     __declspec(property(get = GetDoctor, put = SetDoctor)) clsDoctor Doctor;
 
 
 
-    clsPatient GetPatient()
+    clsPatient GetPatient() const
     {
         return _patient;
     }
-
     void SetPatient(clsPatient patient)
     {
         _patient = patient;
     }
-
     __declspec(property(get = GetPatient, put = SetPatient)) clsPatient Patient;
 
-
-    
-    string GetDateTime()
-    {
-        return _dateTime;
+    float GetBasePrice() const  {
+        return this->_basePrice;
     }
-
-    void SetDateTime(string dateTime)
-    {
-        _dateTime = dateTime;
+    void SetBasePrice(float basePrice) {
+        this->_basePrice = basePrice;
     }
+    __declspec(property(get = GetBasePrice, put = SetBasePrice)) float BasePrice;
 
-    __declspec(property(get = GetDateTime, put = SetDateTime)) string DateTime;
+    float GetTotalPrice() const {
+        return this->_basePrice * (1 + this->_doctor.FeesRate / 100);
+    }
+    __declspec(property(get = GetTotalPrice)) float TotalPrice;
+
+    float GetAppointmentFees() const {
+        return this->_appointmentFees;
+    }
+    void GetAppointmentFees(float appointmentFees) {
+        this->_appointmentFees = appointmentFees;
+    }
+    __declspec(property(get = GetAppointmentFees, put = GetAppointmentFees)) float AppointmentFees;
+
+    bool IsAppointmentFeesEqualTotalPrice() {
+        return this->_appointmentFees == TotalPrice;
+    }
+    bool IsEmpty() {
+        return this->_mode == clsTemplate<clsAppointment>::enMode::eEmptyMode;
+    }
+    static clsAppointment GetEmptyAppointmentObject() {
+        return clsAppointment();
+    }
+    static clsAppointment GetNewAppointmentObject() {
+        return clsAppointment(clsTemplate<clsAppointment>::enMode::eAddNewMode,clsPatient(),clsDoctor(),0.0);
+    }
+    static map<string, clsAppointment> GetAllAppointment() {
+        return  _loadAppointmentDataFromFile();
+    }
+    static clsAppointment FindAppointment(string appointmentID) {
+        return clsTemplate<clsAppointment>::FindObject(appointmentID, _loadAppointmentDataFromFile, GetEmptyAppointmentObject);
+    }
+    bool Delete() {
+        return clsTemplate<clsAppointment>::DeleteObject(*this,
+            this->_mode, this->_objectIsSaved,
+            [this]()-> void {_UpdateAppointment(); },
+            GetEmptyAppointmentObject);
+    }
+    bool Save() {
+        return clsTemplate<clsAppointment>::Save(
+        this->_mode, [this]()-> void {_UpdateAppointment(); },
+            [this]()-> void {_AddAppointmentToFile(); },this->_objectIsSaved);
+    }
 };
