ac6a85504f7d4b783ee11510b99a3f1ff77fb694
Loay Alarify <loayalarify777184509@gmail.com>
2026-04-29 05:36:50 +0300

    feat(patient): add transaction logic for CRUD operations

--- ProjectFiles/Core/clsPatient.h
+++ ProjectFiles/Core/clsPatient.h
@@ -1,12 +1,20 @@
 #pragma once
-#include "clsDoctor.h"
-#include"clsPerson.h"
+#include "..//Libraries/clsDate.h"
+#include"..//Core/clsPerson.h"
+#include "..//Database/FilesName.h"
+#include<vector>
+#include<fstream>
+#include"..//Libraries/clsString.h"
+#include<map>
+#include"clsTemplate.h"
 class clsPatient :public clsPerson
 {
 public:
-    enum enGender {
-        eFemale = 0,
-        eMale = 1
+    enum enGender
+    {
+        eMale = 1,
+        eFemale,
+        eUnkownGender
     };
 
 private:
@@ -15,22 +23,132 @@ private:
     string _patientID;
     enGender _gender;
     clsDate _birthdate;
-    clsDoctor _doctor;
-    float _bookingFee;
+    clsTemplate<clsPatient>::enMode _mode;
+    clsTemplate<clsPatient>::enIsSave  _ObjectIsSaved;
 
-public:
-    clsPatient(){}
-    clsPatient(string PatientID, string firstName, string secondName, string thirdName, string fourthName, enGender gender, clsDate birthdate, string phone, clsDoctor doctor, float bookingFee) 
-        :clsPerson(firstName,secondName,thirdName,fourthName,phone)
+
+
+    string _ConvertGenderToString(enGender patientGender) {
+
+
+        return (patientGender == enGender::eMale) ? "male" :
+            (patientGender == enGender::eFemale) ? "female" : "No Gender";
+
+
+    }
+    string _ConvertPatientToDataLine(const clsPatient& patientInfo, string separator = "#//#") {
+
+        string line;
+
+        line = patientInfo.PatientID + separator;
+        line += patientInfo.FirstName + separator;
+        line += patientInfo.SecondName + separator;
+        line += patientInfo.ThirdName + separator;
+        line += patientInfo.FourthName + separator;
+        line += _ConvertGenderToString(patientInfo.Gender) + separator;
+        line += clsDate::DateToString(patientInfo.Birthdate) + separator;
+
+        line += patientInfo.Phone;
+
+    
+
+
+        return line;
+    }
+    static   enGender _ConvertFromStringToGender(string gender) {
+
+
+        return (gender == "male") ? enGender::eMale : enGender::eFemale;
+    }
+
+    static  clsPatient _ConvertDataLineToPatient(string DataOfLine, string separators) {
+
+        vector<string>vLines;
+
+        vLines = clsString::SpilitString(DataOfLine, separators);
+
+        clsPatient patient(
+            clsTemplate<clsPatient>::enMode::eUpadateMode,
+            vLines[0],
+            vLines[1],
+            vLines[2],
+            vLines[3],
+            vLines[4],
+            _ConvertFromStringToGender(vLines[5]),
+            clsDate::StringToDate(vLines[6]),
+            vLines[7] 
+        );
+       
+        return patient;
+
+    }
+    static  string getID(const clsPatient& p) {
+        return p.PatientID;
+    }
+    static map<string, clsPatient> _LoadPatientFromFiles() {
+        return clsTemplate<clsPatient>::LoadObjectsDataFromFiles(PatientFile, _ConvertDataLineToPatient, getID);
+
+    }
+    clsTemplate<clsPatient>::enMode getMode(const clsPatient& patient) {
+
+        return patient._mode;
+    }
+    void _SavePatientDataToFile(const map<string, clsPatient>& patientDataMap) {
+
+        clsTemplate<clsPatient>::SaveObjectsDataToFile(PatientFile, patientDataMap,
+            [this](const clsPatient& tempPatient)-> clsTemplate<clsPatient>::enMode { return getMode(tempPatient); }
+        , [this](const clsPatient& tempPatient, string sperator = "#//#")-> string {return _ConvertPatientToDataLine(tempPatient, sperator); });
+
+    }
+    void _UpdatePatient() {
+
+        clsTemplate<clsPatient>::UpdateObject(_LoadPatientFromFiles, *this, getID,
+            [this](const map<string, clsPatient>& patientDataMap)->void {_SavePatientDataToFile(patientDataMap); },
+            _ObjectIsSaved);
+
+    }
+    string  _GetPatientNumber() {
+        return clsTemplate<clsPatient>::GetEndNumberFromFile(_LoadPatientFromFiles,9);
+
+    }
+    void _GeneratingIDForObject() {
+
+        this->_patientID += _GetPatientNumber();
+    }
+
+
+    void _AddPatientToFile() {
+
         
+        clsTemplate<clsPatient>::AddObjectToFile(PatientFile, *this,
+            [this](const clsPatient& tempPatient, string sperator = "#//#")-> string {return _ConvertPatientToDataLine(tempPatient, sperator); }
+        , [this](void)->void {_GeneratingIDForObject(); }, _ObjectIsSaved);
+    }
+    clsPatient(clsTemplate<clsPatient>::enMode mode,string patientID ,string firstName, string secondName, string thirdName, string fourthName, enGender gender, clsDate birthdate, string phone) 
+    :clsPerson(firstName, secondName, thirdName, fourthName, phone){
+        this->_patientID = patientID;
+       this-> _mode = mode;
+       this-> _gender = gender;
+      this->  _birthdate = birthdate;
+       this-> _ObjectIsSaved = clsTemplate<clsPatient>::enIsSave::DataisUnSaved;
+
+
+
+    }
+public:
+    clsPatient() :clsPatient(clsTemplate<clsPatient>::enMode::eEmptyMode,"Patient00", "", "", "", "", enGender::eUnkownGender, clsDate(0, 0, 0), "") {
+
+
+    };
+
+    
+    
+    clsPatient(clsTemplate<clsPatient>::enMode mode, string firstName, string secondName, string thirdName, string fourthName, enGender gender, clsDate birthdate, string phone)
+        :clsPatient(mode, "Patient00", firstName, secondName, thirdName, fourthName, gender, birthdate, phone)
     {
 
-        _patientID = PatientID;
-        _gender = gender;
-        _birthdate = birthdate;
-        _doctor = doctor;
-        _bookingFee = bookingFee;
-
+    
+      
     };
 
     string GetPatientID() const
@@ -61,27 +179,50 @@ public:
 
     __declspec(property(get = GetBirthdate, put = SetBirthdate))clsDate Birthdate;
 
-
-    void SetBookingFee(float bookingFee)
-    {
-        _bookingFee = bookingFee;
+    string GetDateforYears() {
+        return to_string(Birthdate.CalculateMyAgeInDays() / 365);
     }
-    float GetBookingFee() const
-    {
-        return _bookingFee;
+    __declspec(property(get = GetDateforYears)) string Age;
+
+    string GenderToString() {
+        return _ConvertGenderToString(this->_gender);
     }
-    __declspec(property(get = GetBookingFee, put = SetBookingFee))float bookingFee;
+    __declspec(property(get = GenderToString)) string strGender;
+    bool IsEmpty() {
 
-    clsDoctor GetDoctor()const {
-
-
-        return _doctor;
+        return (_mode == clsTemplate<clsPatient>::enMode::eEmptyMode);
     }
-    clsDoctor SetDoctor(clsDoctor doctor) {
-
-
-        _doctor = doctor;
+    static   map<string, clsPatient> GetAllPatients() {
+        return _LoadPatientFromFiles();
     }
-    __declspec(property(get = GetDoctor, put = SetDoctor))clsDoctor Doctor;
+
+    static clsPatient GetNewPatientObject() {
+
+        return clsPatient(clsTemplate<clsPatient>::enMode::eAddNewMode, "", "", "",
+            "", enGender::eUnkownGender, clsDate(0, 0, 0),"");
+    }
+
+    static clsPatient GetEmptyObject() {
+        return  clsPatient(clsTemplate<clsPatient>::enMode::eEmptyMode, "", "", "", "",
+            enGender::eUnkownGender, clsDate(0, 0, 0), "");
+    }
+    static clsPatient FindPatient(string patientID) {
+
+        return   clsTemplate<clsPatient>::FindObject(patientID, _LoadPatientFromFiles, GetEmptyObject);
+
+    }
+    bool Delete() {
+
+        return   clsTemplate<clsPatient>::DeleteObject(*this, this->_mode,
+            this->_ObjectIsSaved, [this]() { _UpdatePatient(); }, GetEmptyObject);
+
+    }
+    bool Save() {
+
+        return clsTemplate<clsPatient>::Save(_mode, [this]() { _UpdatePatient(); },
+            [this]() { _AddPatientToFile(); }, _ObjectIsSaved);
+
+    }
+
 };
 
