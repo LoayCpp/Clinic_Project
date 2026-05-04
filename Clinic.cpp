1de2dd1db99924e26ecd0c68be6f9172bf0ffa31
amr <amralamrei65@gmail.com>
2026-05-04 22:01:28 +0300

    chore(fix-merge-conflicts): just fix the conflicts that was in the clinic.cpp file

--- Clinic.cpp
+++ Clinic.cpp
@@ -1,14 +1,5 @@
 ﻿#include <iostream>
-#include "ProjectFiles//Core//clsDoctor.h"
-#include"ProjectFiles//Core//clsPatient.h"
-#include"ProjectFiles/Core/clsAppointment.h"
-#include"ProjectFiles//Screens//MainMenu/clsMainMenu.h"
-#include"ProjectFiles//Screens/MainMenu/clsManagePatientMenu.h"
-#include"ProjectFiles//Screens/AppointmentMenu/clsShowAppointmentTableScreen.h"
-#include"ProjectFiles//Screens/AppointmentMenu/clsFindAppointmentScreen.h"
-#include"ProjectFiles//Screens/AppointmentMenu/clsAddAppointmentScreen.h"
-#include "ProjectFiles//Screens/AppointmentMenu//clsUpdateAppointmentScreen.h"
-#include"ProjectFiles//Screens//PatientMenu//clsUpdatePaitentsDataScreen.h"
+#include"ProjectFiles//Screens//MainMenu//clsMainMenu.h"
 using namespace std;
 int main() {
 
