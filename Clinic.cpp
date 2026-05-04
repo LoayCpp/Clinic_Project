fd8e06602a12082cdd7b570c535869cc489c1451
amr <amralamrei65@gmail.com>
2026-05-04 21:27:17 +0300

    chore(show-all-patients): i just edit the layout of the patient`s table

--- Clinic.cpp
+++ Clinic.cpp
@@ -1,22 +1,9 @@
 ﻿#include <iostream>
-#include "ProjectFiles//Core//clsDoctor.h"
-#include"ProjectFiles//Core//clsPatient.h"
-#include"ProjectFiles/Core/clsAppointment.h"
-#include"ProjectFiles//Screens//MainMenu/clsMainMenu.h"
-#include"ProjectFiles//Screens/MainMenu/clsManagePatientMenu.h"
-#include"ProjectFiles//Screens/AppointmentMenu/clsShowAppointmentTableScreen.h"
-#include"ProjectFiles//Screens/AppointmentMenu/clsFindAppointmentScreen.h"
-#include"ProjectFiles//Screens/AppointmentMenu/clsAddAppointmentScreen.h"
-#include "ProjectFiles//Screens/AppointmentMenu//clsUpdateAppointmentScreen.h"
-#include"ProjectFiles//Screens//PatientMenu//clsUpdatePaitentsDataScreen.h"
+#include"ProjectFiles//Screens//MainMenu//clsMainMenu.h"
 using namespace std;
 int main() {
 
-   // clsMainMenu::ShowMenu();
-	//clsShowAppointmentTableScreen::ShowAppoitmentsTable();
-	//clsFindAppointmentScreen::ShowFindAppointment();
-	//clsUpdatePaitentsDataScreen::ShowUpdatePatientData();
-	//clsAddAppointmentScreen::ShowAddAppointment();
-	clsUpdateAppointmentScreen::ShowUpdateAppointmentData();
+   clsMainMenu::ShowMenu();
+
    return 0;
 }
